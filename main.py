6ea5deb26e80a80e0a5df310dad38bec2a721988
Jintumoni <jintu.kvs@gmail.com>
2023-03-21 22:33:51 +0530

    code to draw the lines for ordered sequence

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from connected_component import connected_components
 from distanceTransform import DT
 from sequence import arrange
 
-INF = float("inf")
-
 # step 1. loading the image
 img = cv.imread('./photos/skew.png');
 H, W = img.shape[0], img.shape[1]
@@ -21,30 +19,25 @@ gray_scale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 distTransform = DT(BnW_image)
 _, final_img = cv.threshold(distTransform, 10, 255, cv.THRESH_BINARY)
 
-
-cv.imshow('distance transform', distTransform)
-cv.imshow('transformed image', final_img)
-
 # step 4. apply BFS to find contour
 contours = connected_components(final_img)
 for contour in contours:
     color = (0, 0, 0) # black boxes
     cv.rectangle(img, (contour[1], contour[0]), (contour[3], contour[2]), color, thickness=1)
 
-CSF = arrange(contours)
-CSFThreshold = 4.0
-# print(CSF)
+lines = arrange(contours)
+for _ in range(len(lines)):
+    for i in range(0, len(lines[_]) - 1):
+        box1, box2 = lines[_][i], lines[_][i + 1]
+        startPoint = (box1[1] + box1[3]) // 2, (box1[0] + box1[2]) // 2 
+        endPoint = (box2[1] + box2[3]) // 2, (box2[0] + box2[2]) // 2
+        color = (0, 255, 0)
+        thickness = 3
+        cv.line(img, startPoint, endPoint, color, thickness=thickness)
 
-for i in range(len(CSF)):
-    for j in range(len(CSF)):
-        if CSF[i][j] >= CSFThreshold:
-            startPoint = (contours[i][1] + contours[i][3]) // 2, (contours[i][0] + contours[i][2]) // 2 
-            endPoint = (contours[j][1] + contours[j][3]) // 2, (contours[j][0] + contours[j][2]) // 2
-            color = (0, 255, 0)
-            thickness = 3
-            cv.line(img, startPoint, endPoint, color, thickness=thickness)
-
-# print(CSF)
+cv.imshow('distance transform', distTransform)
+cv.imshow('transformed image', final_img)
 cv.imshow('contours', img)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
