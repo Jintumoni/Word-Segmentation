843f75e8bca0d227947bc4ea346567f33ac2beae
Jintumoni <jintu.kvs@gmail.com>
2023-03-21 22:33:19 +0530

    add more DSU methods

--- DSU.py
+++ DSU.py
@@ -3,13 +3,12 @@ class DSU:
         self.parents = [-1 for _ in range(n)]
     
     def find(self,x):
-        if self.parents[x] > 0:
+        if self.parents[x] >= 0:
             return self.find(self.parents[x])
         return x
     
     def union(self, x, y):
         rootA, rootB = self.find(x), self.find(y)
-        print(rootA, rootB, self.parents[rootA], self.parents[rootB])
         if rootA == rootB:
             return 0
         elif abs(self.parents[rootA]) > abs(self.parents[rootB]):
@@ -20,3 +19,13 @@ class DSU:
             self.parents[rootA] = rootB
             
         return 1
+    
+    def distinctParents(self):
+        cnt = 0
+        for _ in self.parents:
+            cnt += 1 if _ < 0 else 0
+        return cnt
+    
+    def hasParent(self, x):
+        return False if self.parents[x] == -1 else True
+    
